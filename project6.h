d27d91b61931b5681c65a28b3c404e54aea4f225
Andrew Audrain <43102350+thedraino@users.noreply.github.com>
2018-11-21 15:49:22 -0600

    Update project6.h
    Added prototype for signal handling. Added global const for the maximum processes to be alive at one time. Added struct for message.

--- project6.h
+++ project6.h
@@ -27,11 +27,24 @@
 #include <stdbool.h>
 
 /* Macro variables */
+define MAX_CURRENT_PROCESSES 18
 
 /* Structures */
-
+// Structure used in the message queue 
+typedef struct {
+	long msg_type;		// Controls who can receive the message.
+	int pid;		// Store the pid of the sender.
+	int tableIndex;		// Store the index of the child process from USER.
+	int request;		// Some value from 0-19 if the child process is requesting a resource from OSS.
+	int release;		// Some value from 0-19 if the child is notifying OSS that it is releasing a resource.
+	bool terminate;		// Default is false. Gets changed to true when child terminates. 
+	bool resourceGranted;	// Default is false. Gets changed to true when OSS approves the resource request from USER.
+	unsigned int messageTime[2];	// Will store the simulated clock's time at the time a message is sent
+} Message;
+  
 /* Shared memory info */
 
 /* Function prototypes */
+void handle ( int sig_num );
 
 #endif
