8cfba493a5e29a898a2c07c605e0f86f852a833f
Andrew Audrain <43102350+thedraino@users.noreply.github.com>
2018-11-21 14:25:21 -0600

    Update project6.h
    Added libraries that needed to be included for both files.

--- project6.h
+++ project6.h
@@ -3,3 +3,35 @@
 // Header file
 // Created by: Andrew Audrain
 // Created on: 11/21/2018
+//
+// Header file for any information or resources needed in both oss.c and user.c
+
+#ifndef PROJECT6_HEADER_FILE
+#define PROJECT6_HEADER_FILE
+
+/* Included libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+/* Macro variables */
+
+/* Structures */
+
+/* Shared memory info */
+
+/* Function prototypes */
+
+#endif
