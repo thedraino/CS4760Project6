73b51d0b9bb2bc66574d3b2983f0607211d8dd63
Andrew Audrain <43102350+thedraino@users.noreply.github.com>
2018-11-21 14:29:55 -0600

    Update user.c
    Included the project6 header file. Added basic printf statement to test Makefile.

--- user.c
+++ user.c
@@ -3,3 +3,13 @@
 // Executable: user
 // Created by: Andrew Audrain
 // Created on: 11/21/2018
+//
+// Purpose...
+
+#include "project6.h"
+
+int main ( int argc, char *argv[] ) {
+	printf ( "Hello, from USER.\n" );
+	
+	return 0;
+}
