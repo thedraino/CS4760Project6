45e34d450164726070b0f75c945fb83d9d921e61
Andrew Audrain <43102350+thedraino@users.noreply.github.com>
2018-12-05 01:57:39 -0600

    Update oss.c
    Added main loop logic. Need to work in user.c and test.

--- oss.c
+++ oss.c
@@ -41,8 +41,8 @@ typedef struct {
 /***** Function Prototypes *****/
 // General Functions
 void manageClock ( unsigned int clock[] , int timeElapsed );
-void clearPCBEntry ( int processID );
-void clearFrameEntries ( int processID);
+// void clearPCBEntry ( int processID );
+// void clearFrameEntries ( int processID);
 void printReport();
 void cleanUpResources();
 
@@ -75,7 +75,6 @@ unsigned int totalSecondsProgramRan;
 // Logfile information
 FILE *fp;
 char logName[12] = "program.log";
-int numberOfLines; 
 
 
 /*************************************************************************************************************/
@@ -91,10 +90,11 @@ int main ( int argc, char *argv[] ) {
 	int maxCurrentProcesses;	// Controls the maximum number of processes that can be running at one time.
 	int maxTotalProcesses = 100;	// Controls the maximum number of processes that can be created throughout the program. 
 	long ossID = getpid();		// Store the pid for OSS.
-	unsigned int newProcessTime[2] = { 0, 0 };	// Time value at which a new process should be be created.
-	fp = fopen ( logName, "w+" );	// Open up the log file for writing to. 
-	numberOfLines = 0;
 	srand ( time ( NULL ) );	// Seed for random number generation.
+	
+	fp = fopen ( logName, "w+" );	// Open up the log file for writing to. 
+	bool keepLogging = true;	// Flags if the log file has reached its line limit. 
+	int numberOfLines = 0;		// Tracks the number of lines in the log file. 
 	
 	/* Getopts */
 	// Loop to implement getopt to get any command-line options and/or arguments.
@@ -171,6 +171,9 @@ int main ( int argc, char *argv[] ) {
 	}
 	
 	/* Setup for main loop */
+	// Create a queue large enough to hold all of the frames in the frame table at once. 
+	Queue* queue = createQueue ( MAX_MEMORY );
+	
 	// Frame Table
 	// Declare the struct. Set the occupied bit for each index to 0. 
 	Frame frameTable[MAX_MEMORY];
@@ -180,6 +183,7 @@ int main ( int argc, char *argv[] ) {
 	
 	// Process Control Block
 	// Declare the struct. Set the pid for each index to 0. Set each page's value to -1. 
+	int pcbIndex = 0; 
 	Process processControlBlock[maxCurrentProcesses];
 	for ( i = 0; i < maxCurrentProcesses; ++i ) {
 		processControlBlock[i].pid = 0;
@@ -188,30 +192,325 @@ int main ( int argc, char *argv[] ) {
 		}
 	}
 	
-	// Various boolean variables used throughout main loop. 
+	// Various variables used throughout main loop. 
+	pid_t childPid;
+	int currentIndex;
+	int frameIndex;
 	bool messageReceived;	// Flags if OSS has received a message.
 	bool pagePresent;	// Flags if the requested page is already stored in the frame table.
 	bool frameAvailable;	// Flags if there is an open frame in the frame table.
 	bool noEmptyFrame; 	// Flags if the frame table is full. 
+	bool createProcess;	// Flags if it is okay to create a new process.
+	unsigned int newProcessTime[2] = { 0, 0 };	// Time value at which a new process should be be created.
 	
-	// Create a queue large enough to hold all of the frames in the frame table at once. 
-	Queue* queue = createQueue ( MAX_MEMORY );
+	/* Main Loop */
+	while ( 1 ) {
+		/* 1. Check to make sure the log file has surpassed it maximum number of lines allowed. 
+		If it has, close the file and set the flag to false so no more writes will be done. */
+		if ( numberOfLines >= 10000 ) {
+			keepLogging = false; 
+			fclose ( fp );
+		}
+		
+		/* 2. Check to see if it is time to create a new process (shmClock needs to have passed
+		the time stored in newProcessTime). */
+		if ( ( shmClock[0] == newProcessTime[0] && shmClock[1] >= newProcessTime[1] ) || 
+		    shmClock[0] >= newProcessTime[0] ) {
+			createProcess = false;	// Flag is reset to false each run through the loop. 
+			
+			// Check to make sure there is an available location in the process control block. 
+			for ( i = 0; i < maxCurrentProcesses; ++i ) {
+				if ( processControlBlock[i].pid == 0 ) {
+					createProcess = true; 
+					pcbIndex = i;	// Set the current index of the process control block
+					childPid = fork();
+					break;
+				}
+			} // End of checking for room in process control block
+			
+			// If there is room...
+			if ( createProcess == true ) {
+				// Error check...
+				if ( childPid == -1 ) {
+					perror ( "OSS: Failure to fork process correctly." );
+					kill ( getpid(), SIGINT );
+				}
+				
+				// Inside the child process...
+				if ( childPid == 0 ) {
+					execl ( "./user", "user", NULL );
+				}
+				
+				// Inside OSS...
+				processControlBlock[pcbIndex].pid = childPid; 
+				processControlBlock[pcbIndex].processCreationTime[0] = shmClock[0];
+				processControlBlock[pcbIndex].processCreationTime[1] = shmClock[1];
+				totalProcessCreated++;
+				
+				// Generate new time for the next process to be created. 
+				newProcessTime[0] = shmClock[0]; 
+				newProcessTime[1] = ( rand() %  5000000 ) + 1000000 + shmClock[1]; 
+				newProcessTime[0] += newProcessTime[1] / 1000000000;
+				newProcessTime[1] = newProcessTime[1] % 1000000000;
+			}
+		} // End of creating new process
+		
+		/* 3. Wait for a message from a child with a memory request. */
+		msgrcv ( messageID, &message, sizeof ( message ), getpid(), 1 );
+		totalMemoryRequests++;
+// 		printf ( "Message received.\n" );
+
+		
+		// Set the following flags to false each run through the loop...
+		noEmptyFrames = false; 
+		
+		/* 4. Get the index for the process in the process control block */
+		for ( i = 0; i < maxCurrentProcesses; ++i ) {
+			if ( processControlBlock[i].pid == message.pid ) {
+				currentIndex = i; 
+				break;
+			}
+		}
+		
+		/* 5. Check for termination notice from USER. */
+		if ( message.terminate == true ) {
+			if ( keepLogging ) {
+				fprintf( fp, "OSS: Process %d terminated at %d:%d\n", message.pid, 
+				       message.sentTime[0], message.sentTime[1] );
+				numberOfLines++;
+			}
+			kill ( message.pid, SIGTERM ); 
+			wait ( NULL );
+			continue; 
+		} else {
+			if ( keepLogging ) {
+				fprintf( fp, "OSS: Process %d is requesting access to Page %d at %d:%d\n", 
+				       message.pid, message.pageReferenced, shmClock[0], shmClock[1] );
+				numberOfLines++;
+			}
+		}
+		
+		/* 6. Check for page fault */
+		// Need to check search the process's page table for the correct mapping of the requested
+		//	page to its location in the frame table. 
+		// If the page is present...
+		pagePresent = false; 
+		if ( processControlBlock[currentIndex].pageTable[message.pageReferenced] != -1 ) {
+			pagePresent = true; 
+			
+			// Set the index within the page table that maps to the appropriate frame. 
+			frameIndex = processControlBlock[currentIndex].pageTable[message.pageReferenced];
+			
+			// Set reference bit for the frame back to 1 since it was recently referenced. 
+			frameTable[frameIndex].referenceBit = 1; 
+		
+			// If request_type was a write, set the frame's dirty bit. 
+			if ( message.request_type == WRITE ) {
+				if ( keepLogging ) {
+					fprintf( fp, "OSS: Process %d is writing to Page %d in Frame %d\n", 
+						message.pid, message.pageReferenced, frameIndex );
+					numberOfLines++;
+				}
+				frameTable[frameIndex].dirtyBit = 1; 
+			}
+			
+			// If request_type was a write, increment the clock based on if the frame's dirty bit is 
+			//	set or not. A set dirty bit will result in slight processing time as opposed to 
+			//	requesting to read a blank frame. 
+			if ( message.request_type == READ ) {
+				if ( frameTable[frameIndex]dirtyBit == 1 ) {
+					if ( keepLogging ) {
+						fprintf( fp, "OSS: Process %d is reading from Page %d (dirty) in Frame %d\n", 
+							message.pid, message.pageReferenced, frameIndex );
+						numberOfLines++;
+					}
+					manageClock ( shmClock , 50 );
+				} else {
+					if ( keepLogging ) {
+						fprintf( fp, "OSS: Process %d is reading from Page %d in Frame %d\n", 
+							message.pid, message.pageReferenced, frameIndex );
+						numberOfLines++;
+					}
+				}
+			}
+			manageClock ( shmClock, 10 );
+		} // End of 6.1
+		
+		// If page is not present...
+		if ( !pagePresent ) {
+			totalPageFaults++; 
+			if ( message.request_type == WRITE ) {
+				if ( keepLogging ) {
+					fprintf( fp, "OSS: Process %d requested to write to Page %d at %d:%d\n", 
+						message.pid, message.pageReferenced, shmClock[0], shmClock[1] );
+					fprintf( fp, "OSS: Process %d's Page %d is not in a frame. PAGE FAULT.\n",
+						message.pid, message.pageReferenced );
+					numberOfLines++;
+					numberOfLines++;
+				}
+			}
+			
+			if ( message.request_type == READ ) {
+				if ( keepLogging ) {
+					fprintf( fp, "OSS: Process %d requested to read from Page %d at %d:%d\n", 
+						message.pid, message.pageReferenced, shmClock[0], shmClock[1] );
+					fprintf( fp, "OSS: Process %d's Page %d is not in a frame. PAGE FAULT.\n",
+						message.pid, message.pageReferenced );
+					numberOfLines++;
+					numberOfLines++;
+				}
+			}
+			
+			// Check to see if there is currently room in the frame table for a page to be stored. 
+			for ( i = 0; i < MAX_MEMORY; ++i ) {
+				// If current frame is unoccupied... 
+				if ( frameTable[i].occupiedBit == 0 ) {
+					// Initialize characteristics to reflect new process/page info.
+					frameTable[i].occupiedBit = 1; 
+					frameTable[i].processID = message.pid;
+					frameTable[i].processPageFrame = message.pageReferenced;
+					frameTable[i].referenceBit = 1;
+					frameTable[i].dirtyBit = 0;
+					
+					// Update the process's page table with the coordinating index from the frame table.
+					processControlBlock[currentIndex].pageTable[message.pageReferenced] = i; 
+					
+					// Add this frame index to the queue to track the order in which frames are populated.
+					enqueue ( queue, i );
+					
+					manageClock ( shmClock, 500 ); 
+					
+					// Update log file.
+					if ( keepLogging ) {
+						fprintf( fp, "OSS: Process %d's Page %d was loaded into Frame %d at %d:%d.\n", 
+							message.pid, message.pageReferenced, i );
+						if ( message.request_type == WRITE ) {
+							fprintf( fp, "OSS: Process %d is writing to Page %d in Frame %d\n", 
+								message.pid, message.pageReferenced, i );
+							numberOfLines++;
+						}
+						if ( message.request_type == READ ) {
+							fprintf( fp, "OSS: Process %d is reading from Page %d in Frame %d\n", 
+								message.pid, message.pageReferenced, i );
+							numberOfLines++;
+						}
+						numberOfLines++;
+					} 
+					
+					manageClock ( shmClock, 1500000 ); 
+					break; 
+				} 
+				// If not already-empty frame was found, set the flag to true. 
+				if ( i == ( MAX_MEMORY - 1 ) ) {
+					noEmptyFrames = true;
+				}
+			} // End of logic regarding having found an empty frame		
+			
+			// If there are no empty frames found during the above loop, then the second-chance algorithm
+			//	is used to clear one of the frames to make room for this new request. See README for 
+			//	more info on how the algorithm works in general. 
+			if ( noEmptyFrames == true ) {
+				bool searching = true;	// Flag to control when the search is over.
+				int potentialFrame;	// Temp index of the frameTable to use while searching. 
+				int emptiedFrame;	// Index of the frameTable that will be removed to make room. 
+				
+				// Loop to simulate the second-chance algorithm. 
+				do {
+					// Looks at frames based on the order in which they were occupied. (Essentially sorts
+					// from oldest to newest info). 
+					potentialFrame = front ( queue );
+					dequeue ( queue );
+					
+					// Looks at the referenceBit value. If the bit is set to 1, then the bit is reset
+					// 	to 0, and then put back into the queue (second-chance).
+					if ( frameTable[potentialFrame].referenceBit == 1 ) {
+						frameTable[potentialFrame].referenceBit = 0; 
+						manageClock ( shmClock, 500 );
+						enqueue ( queue, potentialFrame );
+					}
+					
+					// If the bit is set to 0, then the oldest frame having been referenced the least
+					//	recently is selected to be removed from the frame table. 
+					if ( frameTable[potentialFrame].referenceBit == 0 ) {
+						emptiedFrame = potentialFrame;
+						searching = false; 
+						manageClock ( shmClock, 450 );
+						break;
+					}										
+				} while ( searching ); // End of do-while loop
+				
+				// Update the system structures now that a frame has been chosen. 
+				int removedPageOwner; 
+				for ( i = 0; i < maxCurrentProcesses; ++i ) {
+					if ( processControlBlock[i].pid == frameTable[emptiedFrame].processID ) {
+						removedPageOwner = i; 
+						break;
+					}
+				}
+				
+				// Reset the page table index for the removed frame reference. 
+				processControlBlock[removedPageOwner].pageTable[frameTable[emptiedFrame].processPageFrame] = -1;
+				
+				// Update frame with new page info. 
+				frameTable[emptiedFrame].occupiedBit = 1; 
+				frameTable[emptiedFrame].processID = message.pid;
+				frameTable[emptiedFrame].processPageFrame = message.pageReferenced;
+				frameTable[emptiedFrame].referenceBit = 1;
+				frameTable[emptiedFrame].dirtyBit = 0;				
+
+				// Update page table with new frame mapping.
+				processControlBlock[currentIndex].pageTable[message.pageReferenced] = emptiedFrame; 
+				
+				// Put newly occupied frame back in the queue. 
+				enqueue ( queue , emptiedFrame );
+				
+				// Update log. 
+				if ( keepLogging ) {
+					fprintf( fp, "OSS: Clearing Frame %d and swapping in Process %d Page %d.\n", 
+						potentialFrame, message.pid, message.pageReferenced );
+					numberOfLines++;
+				}
+				
+				if ( message.request_type == WRITE ) {
+					if ( keepLogging ) {
+						fprintf( fp, "OSS: Process %d is writing to Page %d in Frame %d at %d:%d\n", 
+							message.pid, message.pageReferenced, frameIndex, shmClock[0], shmClock[1]  );
+						numberOfLines++;
+					}
+					frameTable[frameIndex].dirtyBit = 1; 
+				}
+				
+				if ( message.request_type == READ ) {
+					if ( frameTable[frameIndex]dirtyBit == 1 ) {
+						if ( keepLogging ) {
+							fprintf( fp, "OSS: Process %d is reading from Page %d (dirty) in Frame %d at %d:%d.\n", 
+								message.pid, message.pageReferenced, frameIndex, shmClock[0], shmClock[1] );
+							numberOfLines++;
+						}
+						manageClock ( shmClock , 50 );
+					} else {
+						if ( keepLogging ) {
+							fprintf( fp, "OSS: Process %d is reading from Page %d in Frame %d at %d:%d.\n", 
+								message.pid, message.pageReferenced, frameIndex, shmClock[0], shmClock[1] );
+							numberOfLines++;
+						}
+					}
+				}
+			} // End of implemenation of second-chance algorithm
+			
+		} // End of 6.2 
+		
+		/* 7. Send a message to the child to inform it the memory request was granted. */
+		message.msg_type = message.pid; 
+		
+		if ( msgsnd ( messageID, &message, sizeof ( message ), 0 ) == -1 ) {
+			perror ( "OSS: Failure to send message to USER process." );
+		}
+		
+	} // End of main loop
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	wait ( NULL ); 
+	cleanUpResources();
 	
 	return 0;
 }
@@ -223,11 +522,15 @@ int main ( int argc, char *argv[] ) {
 /*************************************************************************************************************/
 
 
+// if ( keepLogging ) {
+// 	fprintf( fp, "\n", );
+// 	numberOfLines++;
+// }
+
 // Function that increments the clock by some amount of time at different points to simulate processing time or 
 // overhead time. Also makes sure that nanoseconds are converted to seconds if/when necessary.
 void manageClock ( unsigned int clock[], int timeElapsed ) {
-	int processingTime = timeElapsed;
-	clock[1] += processingTime;
+	clock[1] += timeElapsed;
 
 	clock[0] += clock[1] / 1000000000;
 	clock[1] = clock[1] % 1000000000;
@@ -238,18 +541,9 @@ void printReport() {
 
 }
 
-// Function to clear a specific entry in the process control block upon termination of a process that was stored there. 
-void clearPCBEntry ( int processID ) {
-	
-}
-
-// Function to clear any and all entries associated with a process upon termination of the associated process. 
-void clearFrameEntries ( int processID) {
-	
-}
-
 // Function to terminate all shared memory and message queue upon completion or to be used with signal handling.
 void cleanUpResources() {
+	printReport();
 	// Close the file.
 	fclose ( fp );
 	
@@ -267,7 +561,6 @@ void cleanUpResources() {
 void sig_handle ( int sig_num ) {
 	if ( sig_num == SIGINT || sig_num == SIGALRM ) {
 		printf ( "Signal to terminate was received.\n" );
-		printReport();
 		cleanUpResources();
 		kill ( 0, SIGKILL );
 		wait ( NULL );
